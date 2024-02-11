531cb15208116b06b4c78de35c2a5266333fed9e
jsebasti <jsebasti@student.42barcelona.com>
2024-02-11 05:17:53 +0100

    si

---
+++ srcs/requirements/mariadb/tools/initial_db.sql
@@ -0,0 +1,14 @@
+-- Crear la base de datos
+CREATE DATABASE IF NOT EXISTS jsebastidb;
+
+-- Crear el usuario y asignarle una contraseña
+CREATE USER 'jsebasti'@'%' IDENTIFIED BY 'jsebasti123';
+
+-- Otorgar todos los privilegios al usuario en la base de datos
+GRANT ALL PRIVILEGES ON jsebastidb.* TO 'jsebasti'@'%' WITH GRANT OPTION;
+
+-- Actualizar los privilegios
+FLUSH PRIVILEGES;
+
+-- Cambiar la contraseña del usuario root
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('nueva_contraseña');
