f5cf9d1a6d40cfac420476f7510d0397de5fe0d2
jsebasti <jsebasti@student.42barcelona.com>
2024-02-25 17:39:45 -0500

    all

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -4,6 +4,9 @@ service mariadb start
 if [ ! -d /var/lib/mysql/${MYSQL_DATABASE} ]
 then
 	mysql --user=${MYSQL_ROOT_USER} --password=${MYSQL_ROOT_PASSWORD} < /usr/local/bin/initial_db.sql
+else
+	echo "${MYSQL_DATABASE} already crated"
 fi
+
 
 mysqld
