f7a60550d238639f67098f9af90f13f1878cab62
jsebasti <jsebasti@sudent.42barcelona.com>
2024-02-26 13:14:57 +0100

    all

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -4,7 +4,7 @@ if [ ! -f ./wp-config.php ]
 then
 	wp core download --allow-root
 	wp config create --allow-root --dbname="${MYSQL_DATABASE}" --dbuser="${MYSQL_USER}" --dbpass="${MYSQL_PASSWORD}" --dbhost="${MYSQL_HOSTNAME}"
-	wp core install --allow-root --url="${DOMAIN_NAME}" --title="${WP_TITLE}" --admin_user="${WP_ADMIN}" --admin_password="${WP_ADMIM_PASS}"  --admin_email="${WP_ADMIM_EMAIL}"
+	wp core install --allow-root --url="${DOMAIN_NAME}" --title="${WP_TITLE}" --admin_user="${WP_ADMIN}" --admin_password="${WP_ADMIN_PASS}"  --admin_email="${WP_ADMIN_EMAIL}"
 	wp user create ${WP_USER} ${WP_EMAIL} --role='author' --user_pass="${WP_USER_PASS}" --allow-root
 	wp theme install astra --activate --allow-root
 fi
