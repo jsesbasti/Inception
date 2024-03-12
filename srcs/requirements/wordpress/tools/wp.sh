56e2c2d2167509e8103a01d8a6d6be50d3bfb09d
jsebasti <jsebasti@student.42barcelona.com>
2024-03-12 01:24:27 +0100

    theme of wp changed to kubio

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -6,7 +6,7 @@ then
 	wp config create --allow-root --dbname="${MYSQL_DATABASE}" --dbuser="${MYSQL_USER}" --dbpass="${MYSQL_PASSWORD}" --dbhost="${MYSQL_HOSTNAME}"
 	wp core install --allow-root --url="${DOMAIN_NAME}" --title="${WP_TITLE}" --admin_user="${WP_ADMIN}" --admin_password="${WP_ADMIN_PASS}"  --admin_email="${WP_ADMIN_EMAIL}"
 	wp user create ${WP_USER} ${WP_EMAIL} --role='author' --user_pass="${WP_USER_PASS}" --allow-root
-	wp theme install astra --activate --allow-root
+	wp theme install kubio --activate --allow-root
 	sed -i "s/.*WP_CACHE_KEY_SALT.*$/define\( 'WP_CACHE_KEY_SALT', 'jsebasti.42.fr' \);/" /var/www/html/wp-config.php
 	sed -i "s/\"stop editing\" line. \*\//&\ndefine( 'WP_REDIS_HOST', 'redis' );/" /var/www/html/wp-config.php
 	wp plugin install redis-cache --activate --allow-root
