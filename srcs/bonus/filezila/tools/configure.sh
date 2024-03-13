59c50e29b2b94c42844bef39b5cc9264ed022503
jsebasti <jsebasti@sudent.42barcelona.com>
2024-03-13 13:19:07 +0100

    redis and start ftp

---
+++ srcs/bonus/filezila/tools/configure.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+service vsftpd start
+service ufw start
+
+folder="/home/hostinger/ftp/files"
+
+if [ ! -d "$folder" ]; then
+    mkdir -p "$folder"
+
+ufw enable 
+ufw allow 20/tcp 
+ufw allow 21/tcp 
+ufw allow 22/tcp 
+ufw allow 30000:30003/tcp 
+ufw allow 30000:30003/udp
+fi
